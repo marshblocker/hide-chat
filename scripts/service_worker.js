725af1deb22e4f8f44ff3dcb09b4302d96f9d909
marshblocker <gabrielkennethmarinas@gmail.com>
2023-04-03 00:54:19 +0800

    Replace background script with a service worker per Google Chrome Manifest V3; Initialize names in local storage to empty array when extension is first installed.

---
+++ scripts/service_worker.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ names: [] })
+        .then(() => console.log('Initialize names to [].'))
+        .catch(err => console.log(err));
+})
